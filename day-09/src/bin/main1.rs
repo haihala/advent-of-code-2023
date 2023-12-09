50efb69f79d7effdac7d15fe2cf2dbf4965cf2d3
Eero H??ih??l?? <eero.haihala@gmail.com>
2023-12-09 16:09:18 +0200

    Day 9

---
+++ day-09/src/bin/main1.rs
@@ -0,0 +1,47 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("inputs/input.txt").unwrap();
+    println!("{}", compute(input));
+}
+
+fn compute(input: String) -> String {
+    input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|num| num.parse::<isize>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .fold(0, |acc, new| {
+            acc + {
+                let mut stack = vec![new.clone()];
+                while stack.last().unwrap().iter().any(|num| *num != 0) {
+                    // All of them are not zeros, make a new layer
+                    let prev = stack.last().unwrap().clone();
+                    assert!(prev.len() > 1);
+                    stack.push(prev.windows(2).fold(vec![], |mut acc, window| {
+                        acc.push(window[1] - window[0]);
+                        acc
+                    }))
+                }
+
+                stack
+                    .into_iter()
+                    .rev()
+                    .fold(0, |curr, layer| curr + layer.last().unwrap())
+            }
+        })
+        .to_string()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        let input = fs::read_to_string("inputs/example.txt").unwrap();
+        assert_eq!("114", compute(input));
+    }
+}
